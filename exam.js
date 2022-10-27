4bab5abd60885837e5149cae5e059076c4f5b196
Daniel Umukoro <umukorod1@gmail.com>
2022-10-27 15:27:13 +0100

    made little progress

--- exam.js
+++ exam.js
@@ -1,16 +1,18 @@
-const  slide = document.querySelector('.slide-container');
-const contsld = document.querySelectorAll('.slide-container div')
+const  slide = document.querySelector('.main-container');
+const contsld = document.querySelectorAll('.slide-container')
 const next  = document.querySelector('#next')
 const prev  = document.querySelector('#prev')
 console.log(contsld);
 let counter = 1;
-const size = 500;
+const size = 710;
 console.log(size);
 slide.style.tranform = 'translateX(' + (-size*counter)+'px)';
 
 next.addEventListener('click', function(){
-    slide.style.transform = 'transition 0.4s ese-in-out'
+   
+    slide.style.transfogrm = 'transition 0.4s ese-in-out'
     counter++;
+   
     slide.style.transform = 'translateX('+ (-size*counter)+'px)';
     console.log(counter);
     console.log( slide.style.transform )
