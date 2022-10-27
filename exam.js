064495123e36293e812a94db76184a0710f7d901
Daniel Umukoro <umukorod1@gmail.com>
2022-10-27 16:25:00 +0100

    progress

--- exam.js
+++ exam.js
@@ -4,7 +4,7 @@ const next  = document.querySelector('#next')
 const prev  = document.querySelector('#prev')
 console.log(contsld);
 let counter = 1;
-const size = 710;
+const size = 1372;
 console.log(size);
 slide.style.tranform = 'translateX(' + (-size*counter)+'px)';
 
@@ -16,4 +16,27 @@ next.addEventListener('click', function(){
     slide.style.transform = 'translateX('+ (-size*counter)+'px)';
     console.log(counter);
     console.log( slide.style.transform )
+})
+prev.addEventListener('click', function(){
+   
+    slide.style.transfogrm = 'transition 0.4s ese-in-out'
+    counter--;
+   
+    slide.style.transform = 'translateX('+ (-size*counter)+'px)';
+    console.log(counter);
+    console.log( slide.style.transform )
+})
+slide.addEventListener('transitioned', function(){
+    if (contsld[counter].id === 'last'){
+        slide.style.transition ='none';
+        counter = contsld.length - 2;
+        slide.style.transform ='translateX('+ (-size*counter)+'px)'
+    
+    }
+    if (contsld[counter].id === 'first'){
+        slide.style.transition ='none';
+        counter = contsld.length - 2;
+        slide.style.transform ='translateX('+ (-size*counter)+'px)'
+    
+    }
 })
